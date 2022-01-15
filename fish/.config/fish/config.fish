02d9f3a2c3fd56e48bfdf1ef4276a5814149bb24
Andrei Husovschi <husovschi@protonmail.com>
2022-01-15 22:45:54 +0200

    Added Fish config

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+export EDITOR=nvim || export EDITOR=vim
+export LANG=en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+set -gx PATH $HOME/.local/bin /usr/local/bin /opt/chromium /opt/android-sdk/platform-tools /opt/homebrew/bin $PATH
+
